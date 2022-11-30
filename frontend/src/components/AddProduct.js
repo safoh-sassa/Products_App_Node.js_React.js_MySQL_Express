c8940f1cfdd53cebc94a914e30866dba121fec15
Safoh Sassa <32834244+safoh-sassa@users.noreply.github.com>
2022-11-30 22:37:32 +0100

    Update AddProduct.js

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -2,53 +2,58 @@ import { useState } from 'react'
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
- 
+
 const AddProduct = () => {
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState('');
+    const [add, setAdd] = useState(true);
     const navigate = useNavigate();
- 
+
     const saveProduct = async (e) => {
         e.preventDefault();
-        await axios.post('http://localhost:5000/',{
-            title: title,
-            price: price
-        });
-        
-        navigate("/");
+        setAdd(false)
+        if (add) {
+            await axios.post('http://localhost:5000/', {
+                title: title,
+                price: price
+            });
+
+            navigate("/");
+        }
+       
     }
- 
+
     return (
         <div>
-            <form onSubmit={ saveProduct }>
+            <form onSubmit={saveProduct}>
                 <div className="field">
                     <label className="label">Product</label>
-                    <input 
+                    <input
                         className="input"
                         type="text"
                         placeholder="Title"
-                        value={ title }
-                        onChange={ (e) => setTitle(e.target.value) }
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
                     />
                 </div>
- 
+
                 <div className="field">
                     <label className="label">Price</label>
-                    <input 
+                    <input
                         className="input"
                         type="number"
                         placeholder="Price"
-                        value={ price }
-                        onChange={ (e) => setPrice(e.target.value) }
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
                     />
                 </div>
- 
-                <div className="field">
+
+                <div id="saveBtn" className="field">
                     <button className="button is-primary">Save</button>
                 </div>
             </form>
         </div>
     )
 }
- 
+
 export default AddProduct
